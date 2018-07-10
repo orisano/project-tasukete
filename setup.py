66a72cdcbd6dab4a68ef112e7591c19c6136918d
Nao YONASHIRO <owan.orisano@gmail.com>
2018-07-10 22:29:47 +0900

    refactor: improve setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,102 @@
-#coding:utf-8
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from setuptools import setup
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
 
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'tasukete.py'
+DESCRIPTION = 'help me'
+URL = 'https://github.com/orisano/project-tasukete'
+EMAIL = 'owan.orisano@gmail.com'
+AUTHOR = 'Nao YONASHIRO'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = "0.1.0"
+
+REQUIRED = []
+
+EXTRAS = {}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(VERSION))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name="tasukete.py",
-    version="0.1",
-    author="Nao Yonashiro @orisano",
-    author_email="weekendsummer.endless@gmail.com",
-    scripts=["tasukete.py"]
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    scripts=["tasukete.py"],
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
